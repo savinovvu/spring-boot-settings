570d86bf7ae2a42906742b4733f0fa6661eafa12
skorpion <savinov_vu@inbox.ru>
2018-06-09 10:44:19 +0800

    9. spring data rest. Json cycles resolve. ResponseStatus.CORS.

--- src/main/java/ru/inbox/savinov_vu/webrest/repository/UserRepository.java
+++ src/main/java/ru/inbox/savinov_vu/webrest/repository/UserRepository.java
@@ -2,7 +2,7 @@ package ru.inbox.savinov_vu.webrest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.inbox.savinov_vu.web.User;
+import ru.inbox.savinov_vu.webrest.model.User;
 
 
 
